0228ecaf91fda5f81492fb6fb7a54f167acde00c
Yanice Slegers <yanice@dumbledore.local>
2017-06-07 22:27:43 +0200

    initial commit 2.0

---
+++ src/main/java/ui/view/Launcher.java
@@ -0,0 +1,16 @@
+package ui.view;
+
+import javax.swing.*;
+import domain.*;
+import domain.model.Speler;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
+		Speler speler = new Speler(naam);
+
+		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
